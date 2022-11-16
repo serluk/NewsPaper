11d5fd8b929dbea87c30b09af36437885b91733b
User1 <1@g.com>
2022-11-16 19:25:00 +0300

    Execise D3.4

--- news/models.py
+++ news/models.py
@@ -52,6 +52,9 @@ class Post(models.Model):
     def preview(self):
         return self.text[0:124] + '...'
 
+    def __str__(self):
+        return f'{self.title}'
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- news/views.py
+++ news/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class NewsList(ListView):
+    model = Post
+    context_object_name = 'news'
+    template_name = 'news.html'
+
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'article.html'
+    context_object_name = 'article'
